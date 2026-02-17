60d36f77b84487271f7b12acbb7c4bedc5af9a7d
Jorge del Hoyo <delhoyoballestin23@iespabloserrano.com>
2026-02-17 10:36:48 +0100

    con NPX se ve el db.json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp) // Sin versión
+    alias(libs.plugins.kotlin.serialization) // Sin versión
 }
 
 android {
@@ -53,4 +55,23 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // --- COSAS DE LA PRÁCTICA ---
+
+    // 1. SERIALIZATION
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    // 2. ROOM (Versión 2.6.1)
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    ksp("androidx.room:room-compiler:$room_version") // Usamos KSP
+
+    // 3. RETROFIT
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // 4. COIL
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
